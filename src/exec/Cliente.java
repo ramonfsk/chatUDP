5ec052107fc3972bc7cda49ee45f8c8483089a4b
ramonfsk <ramonfsk@gmail.com>
2019-10-10 12:13:07 -0300

    mudança de abstração

--- src/exec/Cliente.java
+++ src/exec/Cliente.java
@@ -5,47 +5,7 @@ import java.net.*;
 import java.util.*;
 
 public class Cliente {
-	
-	private String nome;
-	private Map<Date, String> msgs;
-	
-	public Cliente(String nome) {
-		this.nome = nome;
-		this.msgs = new HashMap<Date, String>();
-	}
-
-	public void addMsg(Date timeStamp, String msg) {
-		this.msgs.put(timeStamp, msg);
-	}
-	
-	public void listarMsgs() {
-		for (Map.Entry<Date, String> pair : msgs.entrySet()) {
-			System.out.println("TimeStamp: "+pair.getKey()+"| Msg: "+ pair.getValue());
-		}
-	}
-
-	public String getNome() {
-		return this.nome;
-	}
-	
 	public static void main(String[] args) {
-		byte[] msg1 = "MSG1".getBytes(), msg2 = "MSG2".getBytes(), msg3 = "CMD|DSCNNT".getBytes();
 		
-		try {
-			DatagramSocket socket = new DatagramSocket();
-			socket.setSoTimeout(3000);
-			DatagramPacket dgSaida = new DatagramPacket(msg1, msg1.length, InetAddress.getByName("localhost"), 12345);
-			socket.send(dgSaida);
-			System.out.println("Msg1 enviada!");
-			dgSaida = new DatagramPacket(msg2, msg2.length, InetAddress.getByName("localhost"), 12345);
-			socket.send(dgSaida);
-			System.out.println("Msg2 enviada!");
-			dgSaida = new DatagramPacket(msg3, msg3.length, InetAddress.getByName("localhost"), 12345);
-			socket.send(dgSaida);
-			System.out.println("Msg3 enviada!");
-			socket.close();
-		} catch (SocketException e) {
-			System.out.println("Timeout!");
-		} catch (IOException e) { e.printStackTrace(); }
 	}
 }
--- src/exec/Servidor.java
+++ src/exec/Servidor.java
@@ -1,26 +1,59 @@
 package exec;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
+import java.util.*;
 
-public class Servidor {
-	public static void main(String[] args) {
-		
-		ArrayList<Cliente> clnts = new ArrayList<Cliente>();
-		byte[] buffer = new byte[1000];
-		
-		try {
-			DatagramSocket socket = new DatagramSocket(12345);
-			DatagramPacket dgEntrada = new DatagramPacket(buffer, buffer.length);
-				socket.receive(dgEntrada);
-				if ((new String(buffer)) == "CMD|DSCNNT") {
-					System.out.println("O cliente "+dgEntrada.getAddress().getHostName()+" desconectou");
-					socket.close();
-				} else {
-					System.out.println("Conexão com o cliente "+dgEntrada.getAddress().getHostName()+":"+dgEntrada.getPort());
-					System.out.println("MSG: "+(new String(buffer)));
+public class Servidor implements Runnable {
+	public final static int PORTA = 7001;
+	private final static int BUFFER = 1024;
+	
+	private DatagramSocket socket;
+	private ArrayList<InetAddress> enderecoClientes;
+	private ArrayList<Integer> portaClientes;
+	private HashSet<String> clientesExistentes; //HashSet garante que não haja repetição de clientes.
+	
+	public Servidor() throws IOException {
+		socket = new DatagramSocket(PORTA);
+		enderecoClientes = new ArrayList<InetAddress>();
+		portaClientes = new ArrayList<Integer>();
+		clientesExistentes = new HashSet<String>();
+	}
+	
+	@Override
+	public void run() {
+		byte[] buffer = new byte[BUFFER];
+		while(true) {
+			try {
+				Arrays.fill(buffer, (byte)0);
+				DatagramPacket pacote = new DatagramPacket(buffer, buffer.length);
+				socket.receive(pacote);
+				
+				String tmpMsg = new String(buffer);//Pegou o conteúdo da msg
+				InetAddress tmpEndereco = pacote.getAddress();//Pegou o endereco IP
+				int tmpPorta = pacote.getPort();//Pegou a porta
+				String id = tmpEndereco.toString() + ":"+tmpPorta;//Criou um id único para o cliente
+				
+				//Adiciona as informacoes dos clientes em arrays
+				if(!clientesExistentes.contains(id)) {
+					clientesExistentes.add(id);
+					enderecoClientes.add(tmpEndereco);
+					portaClientes.add(tmpPorta);
 				}
+				
+				System.out.println(id+" : "+tmpMsg);
+				byte[] data = (id+" : "+tmpMsg).getBytes();
+				for(int i=0; i < enderecoClientes.size(); i++) {
+					
+				}
+				
+			} catch (Exception e) { e.printStackTrace(); }
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			(new Servidor()).run();
 		} catch (IOException e) { e.printStackTrace(); }
 	}
 }
